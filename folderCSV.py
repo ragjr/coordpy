7210cd6c858a05e27daa3b06fb760ba55648384d
ragjr <rallengilbertjr@gmail.com>
2016-11-30 16:17:55 -0700

    Update folderCSV.py

--- folderCSV.py
+++ folderCSV.py
@@ -1,29 +1,47 @@
-import os, csv
+import os, csv, arcpy
 
+############# Create text files with filenames #############
 # metadata
-with open("C:/Users/ragilbert/Documents/folder.csv", 'w') as f:
+x = 0
+with open("C:/Users/ragilbert/Documents/metaCleanup.csv", 'wb', 1) as f:
+    print('Creating metaCleanup.csv')
     writer = csv.writer(f)
-    writer.writerow(['filename']) 
+    writer.writerow(['OBJECTID','filename'])
     for path, dirs, files in os.walk("P:/metadata"):
         for filename in files:
-            writer.writerow([filename])
-			
+            x = x + 1
+            writer.writerow([x,filename])
+
 # zip
-with open("C:/Users/ragilbert/Documents/folder.csv", 'w') as f:
+x = 0
+with open("C:/Users/ragilbert/Documents/zipCleanup.csv", 'wb') as f:
+    print('Creating zipCleanup.csv')
     writer = csv.writer(f)
-    writer.writerow(['filename'])
+    writer.writerow(['OBJECTID','filename'])
     for path, dirs, files in os.walk("P:/zip"):
         for filename in files:
-            writer.writerow([filename])
+            x = x + 1
+            writer.writerow([x,filename])
 
-###########################################################################S
+############# Copy text data into the default geodatabase #############
+# Set local variables to copy 
+ws = 'C:/Users/ragilbert/Documents'
+db = 'C:/Users/ragilbert/Documents/ArcGIS/Default.gdb'
 
-import os, shutil
+# create a list of new tables
+arcpy.env.workspace = ws
+newTables = arcpy.ListTables('*cleanup*')
 
-src_files = os.listdir("P:/metadata")
-check_files = open("C:/Users/ragilbert/Documents/metaRemoval.csv")
-for filename in src_files:
-    full_file_name = os.path.join(src, filename)
-    if (os.path.isfile(full_file_name)):
-        shutil.copy(full_file_name, dest)
+# create a list of old tables
+arcpy.env.workspace = db
+oldTables = arcpy.ListTables('*cleanup*')
 
+# delete existing tables
+print('Deleting current tables.')
+for i in oldTables:
+    print('Deleting ' + i)
+    arcpy.Delete_management(i)
+
+# import both tables
+print("Importing tables to gdb: " + db)
+arcpy.TableToGeodatabase_conversion(newTables, db) # not working
