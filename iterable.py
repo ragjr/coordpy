1f9acfc59c904f191bb27ac31dcd48ee29bc2671
ragjr <rallengilbertjr@gmail.com>
2016-12-15 12:42:01 -0700

    Create iterable.py

---
+++ iterable.py
@@ -0,0 +1,14 @@
+el = [4999.34,4998.04,4997.74,4999.36]
+
+def neighborhood(iterable):
+    iterator = iter(iterable)
+    prev_item = None
+    current_item = next(iterator)  # throws StopIteration if empty.
+    for next_item in iterator:
+        yield (prev_item, current_item, next_item)
+        prev_item = current_item
+        current_item = next_item
+    yield (prev_item, current_item, None)
+    
+    for prev,item,next in neighborhood(el):
+    print prev, item, next
