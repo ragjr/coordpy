f12c85203c00dfc2d79420e4c96ace4c80e8da63
ragjr <rallengilbertjr@gmail.com>
2016-12-15 13:22:27 -0700

    Update iterable.py

--- iterable.py
+++ iterable.py
@@ -1,3 +1,4 @@
+#v1
 el = [4999.34,4998.04,4997.74,4999.36]
 
 def neighborhood(iterable):
@@ -10,5 +11,18 @@ def neighborhood(iterable):
         current_item = next_item
     yield (prev_item, current_item, None)
     
-    for prev,item,next in neighborhood(el):
+for prev,item,next in neighborhood(el):
     print prev, item, next
+
+#v2
+el = iter([[1,4999.34],[2,4998.04],[3,4997.74],[4,4999.36]])
+
+for e in el:
+	e1 = e[1]
+	e2 = el.next()
+	e2 = e2[1]
+	print([e1,e2])
+    
+#results
+#[4999.34, 4998.04]
+#[4997.74, 4999.36]
